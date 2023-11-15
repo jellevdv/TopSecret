08a5a7d3eb12307b1fac635a0fd0166349c9128d
Jelle Vandevyvere <jvandevyvere@gantner.com>
2023-11-15 14:26:14 +0100

    speak out the command

--- screens/VoiceScreen.js
+++ screens/VoiceScreen.js
@@ -1,7 +1,21 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
+import * as Speech from 'expo-speech';
 
 export default function VoiceScreen() {
+  useEffect(() => {
+    // This will be called when the component mounts
+    speakSentence('Door unlocked');
+    
+    return () => {
+      Speech.stop();
+    };
+  }, []);
+
+  const speakSentence = async (sentence) => {
+    await Speech.speak(sentence, { language: 'en' });
+  };
+
   return (
     <View>
       <Text>Voice Screen</Text>
