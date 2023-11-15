c26590c71960b20ff84871273e9cb1882373f6fe
Jelle Vandevyvere <jvandevyvere@gantner.com>
2023-11-15 13:46:58 +0100

    open camera when clicking face tab

--- screens/FaceScreen.js
+++ screens/FaceScreen.js
@@ -1,10 +1,48 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React, { useRef, useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Camera } from 'expo-camera';
 
-export default function FaceScreen() {
+const FaceScreen = () => {
+  const cameraRef = useRef(null);
+  const [hasPermission, setHasPermission] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View>
-      <Text>Face Screen</Text>
+    <View style={styles.container}>
+      <Camera
+        ref={cameraRef}
+        style={styles.camera}
+        type={Camera.Constants.Type.front}
+        onCameraReady={() => console.log('Camera is ready')}
+      />
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  camera: {
+    flex: 1,
+    width: '100%',
+  },
+});
+
+export default FaceScreen;
